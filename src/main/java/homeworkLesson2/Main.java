9823a7722a2894dd571ab0c44012485164e47d93
andre <andreeff.vladimir@yandex.ru>
2023-05-26 16:18:13 +0300

    homeworkLesson2

---
+++ src/main/java/homeworkLesson2/Main.java
@@ -0,0 +1,42 @@
+package homeworkLesson2;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] a = {1, 101, 4, 303, 1005, 2, 7, 3, 15, 9};
+        sort(a);
+        System.out.println(Arrays.toString(a));
+    }
+
+    public static void sort(int[] array) {
+        for(int i = array.length / 2 - 1; i >= 0; i--)
+            heapify(array, array.length, i);
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+            heapify(array, i, 0);
+        }
+    }
+
+    private static void heapify(int[] array, int heapSize, int rootIndex) {
+        int largest = rootIndex;
+        int leftChild = 2 * rootIndex + 1;
+        int rightChild = 2 * rootIndex + 2;
+
+        if (leftChild < heapSize && array[leftChild] > array[largest])
+            largest = leftChild;
+
+        if (rightChild < heapSize && array[rightChild] > array[largest])
+            largest = rightChild;
+
+        if (largest != rootIndex) {
+            int temp = array[rootIndex];
+            array[rootIndex] = array[largest];
+            array[largest] = temp;
+            heapify(array, heapSize, largest);
+        }
+    }
+}
